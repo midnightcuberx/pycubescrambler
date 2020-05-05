0d4f68ecb5e0523808033c34b17ddd92d5e7848b
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-05-06 10:03:39 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,9 +10,7 @@ setup(
   url = 'https://github.com/midnightcuberx/pycubescrambler',   # Provide either the link to your github or to your website
   download_url = 'https://github.com/midnightcuberx/pycubescrambler/archive/v_0.0.1.tar.gz',    # I explain this later on
   keywords = ['CUBING', 'SCRAMBLES', 'RUBIKS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'random',
-      ],
+  install_requires=[],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
