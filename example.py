de83d404efe51d55bf34daa716b3213696d8e5cc
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-05-08 08:17:59 +1200

    Add files via upload
    Added an example for everything

---
+++ example.py
@@ -0,0 +1,61 @@
+from pycubescrambler import nxn,side,bld
+
+#gets 1x1 scramble and prints it out
+scramble=nxn.get1()
+print(scramble)
+
+#gets 2x2 scramble and prints it out
+scramble=nxn.get2()
+print(scramble)
+
+#gets 3x3 scramble and prints it out
+scramble=nxn.get3()
+print(scramble)
+
+#gets 4x4 scramble and prints it out
+scramble=nxn.get4()
+print(scramble)
+
+#gets 5x5 scramble and prints it out
+scramble=nxn.get5()
+print(scramble)
+
+#gets 6x6 scramble and prints it out
+scramble=nxn.get6()
+print(scramble)
+
+#gets 7x7 scramble and prints it out
+scramble=nxn.get7()
+print(scramble)
+
+#gets fmc scramble and prints it out
+scramble=nxn.get_fmc()
+print(scramble)
+
+#gets skewb scramble and prints it out
+scramble=side.get_skewb()
+print(scramble)
+
+#gets pyraminx scramble and prints it out
+scramble=side.get_pyra()
+print(scramble)
+
+#gets clock scramble and prints it out
+scramble=side.get_clock()
+print(scramble)
+
+#gets megaminx scramble and prints it out
+scramble=side.get_mega()
+print(scramble)
+
+#gets 3bld scramble and prints it out
+scramble=bld.get3()
+print(scramble)
+
+#gets 4bld scramble and prints it out
+scramble=bld.get4()
+print(scramble)
+
+#gets 5bld scramble and prints it out
+scramble=bld.get5()
+print(scramble)
