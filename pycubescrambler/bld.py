69ed2d177836188ea97f6216b53a02be5cca805e
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-06-21 17:48:28 +1200

    Update bld.py

--- pycubescrambler/bld.py
+++ pycubescrambler/bld.py
@@ -1,27 +1,21 @@
 import random
-import nxn
+from scrambles import nnn
 
 def get3():
-  scramble=[]
-  scramble3=nxn.get3()
-  scramble.append(scramble3)
   
+  scramble=[]
+  scramble3=nnn.get3()
+  scramble.append(scramble3)
   possi=["","Rw ","Rw' ","Rw2 ","Fw ","Fw2 ","Fw' "]
   possi2=["Uw","Uw'","Uw2",""]
   scramble.append(random.choice(possi))
   scramble.append(random.choice(possi2))
-  
-
   return "".join(scramble)
 
-
-
 def get4():
-  scramble=nxn.get4()
+  scramble=nnn.get4()
   return scramble
 
-
-
 def get5():
-  scramble=nxn.get5()
+  scramble=nnn.get_big_cube(5)
   return scramble
