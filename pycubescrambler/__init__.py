ac578d0895454d98de5c5aaf1a73b3869cd7dfc6
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-07-09 10:09:28 +1200

    Update __init__.py

--- pycubescrambler/__init__.py
+++ pycubescrambler/__init__.py
@@ -1,21 +1,5 @@
-import execjs
-from os import path
-
-def trim(func):
-    def trimmed_func(*args, **kwargs):
-        return func(*args, **kwargs).strip()
-    return trimmed_func
-
-
-curr_dir = path.dirname(path.realpath(__file__))
-
-with open(path.join(curr_dir, 'js/mathlib.js')) as f:
-    mathlib = f.read()
-
-with open(path.join(curr_dir, 'js/scramble.js')) as f:
-    scramble = f.read()
-
-with open(path.join(curr_dir, 'js/sq1.js')) as f:
-    sq1 = f.read()
-
-sq1scrambler   = execjs.compile(mathlib + scramble + sq1)
+def joinscramble(func):
+  def inner(*args,**kwargs):
+    scramble=func(*args,**kwargs)
+    return "".join(scramble)
+  return inner
