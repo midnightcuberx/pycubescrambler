51f64ef368fa96df0b8b8c148defcd650c4ec231
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-07-08 14:12:15 +1200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,15 +2,21 @@ from distutils.core import setup
 setup(
   name = 'pycubescrambler',         # How you named your package folder (MyLib)
   packages = ['pycubescrambler'],   # Chose the same as "name"
-  version = '0.0.1',      # Start with a small number and increase it with every change you make
+  version = '0.2.4',      # Start with a small number and increase it with every change you make
   license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'a python module to generate scrambles for most wca puzzles',   # Give a short description about your library
   author = 'midnightcuberx',                   # Type in your name
-  author_email = 'drcubing27@gmail.com',      # Type in your E-Mail
+  author_email = 'midnightcuberx@gmail.com',      # Type in your E-Mail
   url = 'https://github.com/midnightcuberx/pycubescrambler',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/midnightcuberx/pycubescrambler/archive/v_0.0.1.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/midnightcuberx/pycubescrambler/archive/v_0.2.4.tar.gz',    # I explain this later on
   keywords = ['CUBING', 'SCRAMBLES', 'RUBIKS'],   # Keywords that define your package best
-  install_requires=[],
+  install_requires=[
+        'appdirs',
+        'packaging',
+        'PyExecJS',
+        'pyparsing',
+        'six',
+  ],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
