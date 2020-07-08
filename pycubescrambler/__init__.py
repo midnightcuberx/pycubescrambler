3dec959d0ab27a06daf67bb708e3778cb5ce5c8c
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-07-08 14:08:11 +1200

    Update __init__.py

--- pycubescrambler/__init__.py
+++ pycubescrambler/__init__.py
@@ -9,13 +9,13 @@ def trim(func):
 
 curr_dir = path.dirname(path.realpath(__file__))
 
-with open(path.join(curr_dir, 'js_resources/mathlib.js')) as f:
+with open(path.join(curr_dir, 'js/mathlib.js')) as f:
     mathlib = f.read()
 
-with open(path.join(curr_dir, 'js_resources/scramble.js')) as f:
+with open(path.join(curr_dir, 'js/scramble.js')) as f:
     scramble = f.read()
 
-with open(path.join(curr_dir, 'js_resources/scramble_sq1.js')) as f:
+with open(path.join(curr_dir, 'js/scramble_sq1.js')) as f:
     sq1 = f.read()
 
 sq1scrambler   = execjs.compile(mathlib + scramble + sq1)
