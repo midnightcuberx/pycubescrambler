3504a69da97089d5c7ddc56c3a52cd44b28f5057
Veni <63829007+midnightcuberx@users.noreply.github.com>
2020-07-09 14:49:14 +1200

    Update nxn.py

--- pycubescrambler/nxn.py
+++ pycubescrambler/nxn.py
@@ -1,5 +1,5 @@
 import random
-from . import main, joinscramble
+from . import main, joinscramble,bldify
 
 @joinscramble
 def big(n):
@@ -66,60 +66,105 @@ def get2():
   return scramble
 
 @joinscramble
-def get3():
-  scramble=main.get3()
-  return scramble
+def get3(scramtype="3x3"):
+  def get3():
+    scramble=main.get3()
+    return scramble
+  
+  @bldify(3)
+  def get3bld():
+    scramble=main.get3()
+    return scramble
 
+  def fmc():
+    a=main.get3()
+    a="".join(a).split()
+    list1=["F","F2","F'","B'","B2","B"]
+    list2=["R2","R","R'","L","L'","L2"]
+    list3=["F","F2","F'"]
+    list4=["R2","R","R'"]
+    times=0
+    while (a[0] in list1 and a[1] in list1) or (a[0] in list3) or (a[-1] in list2 and a[-2] in list2) or (a[-1] in list4):
+      if times>3:
+        a=main.get3()
+        a="".join(a).split()
+        times=0
 
-
-def get_fmc():
-  a=main.get3()
-  a="".join(a).split()
-  list1=["F","F2","F'","B'","B2","B"]
-  list2=["R2","R","R'","L","L'","L2"]
-  list3=["F","F2","F'"]
-  list4=["R2","R","R'"]
-  times=0
-  while (a[0] in list1 and a[1] in list1) or (a[0] in list3) or (a[-1] in list2 and a[-2] in list2) or (a[-1] in list4):
-    if times>3:
-      a=main.get3()
-      a="".join(a).split()
-      times=0
-
-    if a[0] in list3:
-      a.remove(a[0])
-      times+=1
-
-    if (a[0] in list1 and a[1] in list1):
-      if a[0] not in list3:
-        a.remove(a[1])
+      if a[0] in list3:
+        a.remove(a[0])
         times+=1
 
-    
-    if a[-1] in list4:
-      a.remove(a[-1])
-      times+=1
-    
-    if (a[-1] in list2 and a[-2] in list2):
-      if a[-1] not in list4:
-        a.remove(a[-2])
+      if (a[0] in list1 and a[1] in list1):
+        if a[0] not in list3:
+          a.remove(a[1])
+          times+=1
+
+      
+      if a[-1] in list4:
+        a.remove(a[-1])
         times+=1
+      
+      if (a[-1] in list2 and a[-2] in list2):
+        if a[-1] not in list4:
+          a.remove(a[-2])
+          times+=1
 
-  scramble="R' U' F " + " ".join(a) + " R' U' F"
-  return scramble
+    scramble="R' U' F " + " ".join(a) + " R' U' F"
+    return scramble
+
+  if scramtype=="3x3":
+    return get3()
+  
+  elif scramtype=="bld":
+    return get3bld()
+  
+  elif scramtype=="fmc":
+    return fmc()
+  
+  else:
+    raise valueError("That type of scramble does not exist")
+
 
 @joinscramble
-def get4():
-  scramble=main.get4()
-  return scramble
+def get4(scramtype="4x4"):
+  def get4():
+    scramble=main.get4()
+    return scramble
+  
+  @bldify(4)
+  def get4bld():
+    scramble=main.get4()
+    return scramble
+  
+  if scramtype=="4x4":
+    return get4()
+  
+  elif scramtype=="bld":
+    return get4bld()
 
-
-
+  else:
+    raise valueError("That type of scramble does not exist")
 
 @joinscramble
-def get5():
-  scramble=main.get5()
-  return scramble
+def get5(scramtype="5x5"):
+  def get5():
+    scramble=main.get5()
+    return scramble
+  
+  @bldify(5)
+  def get5bld():
+    scramble=main.get5()
+    return scramble
+
+  if scramtype=="5x5":
+    return get5()
+  
+  elif scramtype=="bld":
+    return get5bld()
+
+  else:
+    raise valueError("That type of scramble does not exist")
+
 
 @joinscramble
 def get6():
@@ -157,6 +202,21 @@ def get12():
   return scramble
 
 @joinscramble
-def get_big_cube(n=12):
-  scramble=main.big(n)
-  return scramble
+def get_big_cube(n=12,scramtype="big"):
+  def big():
+    scramble=main.big(n)
+    return scramble
+
+  @bldify(n)
+  def bld():
+    scramble=main.big(n)
+    return scramble
+  
+  if scramtype=="big":
+    return big()
+  
+  elif scramtype=="bld":
+    return bld()
+
+  else:
+    raise valueError("That type of scramble does not exist")
